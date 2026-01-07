e078cf818ec5f3c354ecee981d7d39d502a6188a
phakuphaku <phakuphaku@hotmail.com>
2026-01-07 20:05:42 -0300

    Bootstrap Spring Boot project
    (base project)

---
+++ src/main/java/com/challenge/assistant/ChallengeAssitantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAssitantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeAssitantServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/assistant/ChallengeAssitantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAssitantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
